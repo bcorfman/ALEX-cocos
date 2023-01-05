6a382e869b3a8ac1b21a5c3fbf5c2ca5bdf22634
Brandon Corfman <bcorfman@fastmail.fm>
2023-01-05 17:19:21 -0500

    Initial commit

---
+++ assets/tiled_maps/Tileset3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Tileset3" tilewidth="16" tileheight="16" tilecount="4352" columns="64">
+ <image source="Tileset-2.png" width="1024" height="1088"/>
+</tileset>
