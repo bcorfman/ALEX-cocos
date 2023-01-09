9148fd2aef8cbd634b6fa218910dc58619d5fce4
Brandon Corfman <bcorfman@fastmail.fm>
2023-01-08 21:32:16 -0500

    switch to web-desktop; display is cut off

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./CocosCreator.app/Contents/MacOS/CocosCreator --project "." --build "platform=web-mobile" 2>&1
+./CocosCreator.app/Contents/MacOS/CocosCreator --project "." --build "platform=web-desktop" 2>&1
 exit_code=$?
 if [ ${exit_code} -eq 36 ]; then
     exit 0
