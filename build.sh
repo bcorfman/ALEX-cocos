5af4dad2b90433a8c61fb9ec006d8f4be49754e1
Brandon Corfman <bcorfman@fastmail.fm>
2023-01-08 20:34:44 -0500

    move build to bash script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./CocosCreator.app/Contents/MacOS/CocosCreator --project "." --build "platform=web-mobile" 2>&1
+exit_code=$?
+if [ ${exit_code} -eq 36 ]; then
+    exit 0
+else
+    exit $build_result
+fi
